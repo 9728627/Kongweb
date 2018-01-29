523ce430d79942d066484248c6014778d8d76168
sunxiao <xiao_sun@umail.ucsb.edu>
2018-01-28 18:51:33 -0800

    finish basic functions

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -20,6 +20,26 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = "Post was successfully updated"
+      redirect_to article_path(@article)
+    else
+      reder 'edit'
+    end
+  end
+
+  #define index
+  def index
+    @articles = Article.all
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :description)
